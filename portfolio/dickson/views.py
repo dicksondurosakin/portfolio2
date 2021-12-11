20f7d2c68232350ccffad9ff15410e87d8d50d7b
dicksondurosakin <durosakindickson@gmail.com>
2021-12-11 17:22:10 +0100

    My code is working

--- portfolio/dickson/views.py
+++ portfolio/dickson/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, plain_text_content
 import environ
+
 # initialising the environ
+# your environment variable must be in the same folder where your setting.py is
+# spaces are not allowed in env file ensure that there is no space in your env file
 env = environ.Env()
 environ.Env.read_env()
 # Create your views here.
@@ -10,19 +13,20 @@ environ.Env.read_env()
 
 def index(request):
     if request.method == 'POST':
-        message = Mail(from_email='request.POST.get("email")',
-                       to_emails='durosakindickson@gmail.com',
-                       subject='Testing',
-                       plain_text_content='Please work without much hassle',
-                       html_content='<strong>I love that Gbemi girl</strong>')
+        name = request.POST.get("name")
+        sender = request.POST.get("email")
+        body = request.POST.get('message')
+        # if you change the from_email to an email that not registered as the from on your twilio account then it will flag you
+        message = Mail(from_email="durosakindickson@gmail.com",
+                       # when you make an error in typing twillio is just going to say('ForbiddenError' object has no attribute 'message')
+                       to_emails="femimathias39@gmail.com",
+                       subject=f"From {name}",
+                       plain_text_content=f"Sender: {sender}\n\n{body}")
         try:
             sg = SendGridAPIClient(env('sendgridkey'))
             response = sg.send(message)
             print(response.status_code)
-            print(response.body)
-            print(response.headers)
         except Exception as e:
             print(e.message)
-        print("dickson")
-        print(request.POST.get('message'))
+
     return render(request, 'dickson/index.html')
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,6 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('djangosecretkey')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
